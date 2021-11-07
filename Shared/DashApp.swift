c1545df98949de13598c2f9da79c9b20bffa27d6
Rafi Ciesielczuk <rafi.ciesielczuk@skyscanner.net>
2021-11-07 13:37:35 +0000

    Initial Commit

---
+++ Shared/DashApp.swift
@@ -0,0 +1,17 @@
+//
+//  DashApp.swift
+//  Shared
+//
+//  Created by Rafi Ciesielczuk on 07/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct DashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
